17d72f4ae13baefd781ab77a5e41bcdc6fd152d6
talryan <nataliamryan@gmail.com>
2021-02-17 15:10:02 -0800

    HTML/CSS broken

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -1,29 +1,11 @@
-class Games {
-    constructor() {
-        this.games = []
-        this.arrayOfDoggos = [] 
-   }
-}
+
 
    const baseUrl = "http://localhost:3000"
 
-//    getGames() {
-//     return fetch(this.baseUrl).then(response => response.json()
-//     )
-// }
 
 getGames() {
     fetch(this.baseUrl)
     .then(resp => resp.json())
-    .then(games => createGame(game)) 
+    .then(games => createGame(games)) 
     
 }
-//    fetchAndLoadGames() {
-//     this.getGames().then(games => {
-//         games.forEach(game => this.games.push(new Game(game)))
-//         this.renderScores()
-//     })
-
-// }
-
-//
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,3 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const container = document.querySelector('#app-container')
     new App(container)
 })
+
+const baseUrl = "http://localhost:3000"
+const b = document.getElementById('board-container');
