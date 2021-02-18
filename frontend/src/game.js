20abd2bad743d0ac16e860efd5be812769dee081
talryan <nataliamryan@gmail.com>
2021-02-17 18:33:10 -0800

    Game Class Updated.

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -1,5 +1,10 @@
-class Game{
-    constructor(){
-        
+class Game {
+    static allGames = []
+    constructor(id){
+this.id = id
+Game.allGames.push(this)
+console.log(this)
     }
+
+    //instance method to render an object 
 }
