a9dc30e711df8ccaa9ac32bf0d7e50d793c053f3
talryan <nataliamryan@gmail.com>
2021-02-17 19:04:14 -0800

    Dogs & Users fetched.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,10 +7,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 //  create- new user
-const baseUrl
+const baseUrl = "http://localhost:3000"
 function fetchUsers (){
     fetch(`${baseUrl}/users`)
-    .then(response => console.log(response))
+    .then(response => response.json())
+    .then(users => console.log(users) )
 }
 
+function fetchDoggos (){
+    fetch(`${baseUrl}/doggos`)
+    .then(response => response.json())
+    .then(doggos=> console.log(doggos) )
+}
 //create - new game 
