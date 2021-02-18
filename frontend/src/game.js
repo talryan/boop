f595b5039853502b79f3e54bf855559034deca3d
talryan <nataliamryan@gmail.com>
2021-02-17 17:22:46 -0800

    Game Class

--- frontend/src/game.js
+++ frontend/src/game.js
@@ -1,11 +1,5 @@
-
-
-   const baseUrl = "http://localhost:3000"
-
-
-getGames() {
-    fetch(this.baseUrl)
-    .then(resp => resp.json())
-    .then(games => createGame(games)) 
-    
+class Game{
+    constructor(){
+        
+    }
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,5 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.querySelector('#app-container')
-    new App(container)
-})
 
-const baseUrl = "http://localhost:3000"
-const b = document.getElementById('board-container');
+})
+   
